ab683c774fcfbfbb87885c1277181e9a6d2fba5e
GitHub Action <action@github.com>
2022-10-17 03:36:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/oneuplee/androidprojecttemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/oneuplee/androidprojecttemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.oneuplee.androidprojecttemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.oneuplee.androidprojecttemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/oneuplee/androidprojecttemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/oneuplee/androidprojecttemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.oneuplee.androidprojecttemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.oneuplee.androidprojecttemplate.MyBundle
 
 class MyApplicationService {
 
